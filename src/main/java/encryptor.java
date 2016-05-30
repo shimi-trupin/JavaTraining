510577d337c2f8f2156afc2c3caf46c35a6f8678
shimi <shimon.trupin1994@gmail.com>
2016-05-30 21:13:06 +0300

    initial commit

---
+++ src/main/java/encryptor.java
@@ -0,0 +1,10 @@
+/**
+ * Created by shimi on 30/05/2016.
+ * Chapter 1 exercise
+ */
+public class encryptor {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello!");
+    }
+}
