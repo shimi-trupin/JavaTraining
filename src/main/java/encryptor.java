e98e7825e9afeaec5bd87a5e0ce2e193b78bd1b3
shimi <shimon.trupin1994@gmail.com>
2016-06-09 11:36:20 +0300

    initial commit

--- src/main/java/encryptor.java
+++ src/main/java/encryptor.java
@@ -1,3 +1,8 @@
+import com.fasterxml.jackson.core.JsonEncoding;
+
+import java.io.File;
+import java.util.Scanner;
+
 /**
  * Created by shimi on 30/05/2016.
  * Chapter 1 exercise
@@ -5,6 +10,10 @@
 public class encryptor {
     public static void main(String[] args)
     {
-        System.out.println("Hello!");
+        System.out.println("Type 'e' for encryption, or type 'd' for decryption");
+        Scanner scanner = new Scanner(System.in);
+        String src = scanner.nextLine();
+        File file = new File(src);
+        JsonEncoding a = null;
     }
 }
