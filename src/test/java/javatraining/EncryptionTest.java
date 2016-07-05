3357f999d263d7ebdf6e686bb61ea43ab34c9e24
shimi <shimon.trupin1994@gmail.com>
2016-07-05 23:08:30 +0300

    decryprtion

--- src/test/java/javatraining/EncryptionTest.java
+++ src/test/java/javatraining/EncryptionTest.java
@@ -1,9 +1,16 @@
 package javatraining;
 
+import lombok.Cleanup;
+import org.junit.Rule;
 import org.junit.Test;
+
+import java.io.File;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
 
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.instanceOf;
+import org.junit.rules.TemporaryFolder;
 
 /**
  * Created by shimi on 30/06/2016.
@@ -15,6 +22,20 @@ public class EncryptionTest {
         //check that key is generated
         byte b = Encryption.randKey();
         assertThat(b, instanceOf(byte.class));
+
+    }
+
+    @Rule
+    public TemporaryFolder folder= new TemporaryFolder();
+
+    @Test
+    public void encryptionTest() throws Exception {
+        File createdFile = folder.newFile("sampleText.txt");
+
+        @Cleanup PipedOutputStream out = new PipedOutputStream();
+        @Cleanup PipedInputStream in = new PipedInputStream();
+
+        //add functionality test
 
     }
 }
