7aaba1bbfccabe9b9e45918053a4704d347d4981
shimi <shimon.trupin1994@gmail.com>
2016-08-08 15:41:09 +0300

    chapter 7

--- src/main/java/javatraining/Driver.java
+++ src/main/java/javatraining/Driver.java
@@ -1,6 +1,7 @@
 package javatraining;
 
-import javatraining.tools.FileOpener;
+import javatraining.designPatterns.Encryption;
+import javatraining.tools.*;
 
 import javax.xml.XMLConstants;
 import javax.xml.transform.stream.StreamSource;
@@ -8,7 +9,11 @@ import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -24,26 +29,55 @@ public class Driver {
         System.out.println("Choose an action (enter the number):" + "\n" +
                 "1. Encrypt/Decrypt a single file" + "\n" +
                 "2. Encrypt/Decrypt a directory" + "\n" +
-                "3. Additional javatraining.algorithms" + "\n" +
+                "3. Additional algorithms" + "\n" +
                 "4. Change default algorithm");
 
         Scanner scanner = new Scanner(System.in);
         int action = scanner.nextInt();
+
+        /*----Declaring Variables----*/
         String encrypt_decrypt;
+        Encryption encryption;
+        List<Byte> key;
+        EncryptionResult encryptionResult;
+        byte[] data;
+        String path;
+        FileCreator fileCreator;
+        /*---------------------------*/
 
         switch (action){
             case 1:
                 // Encrypt/Decrypt a single file
                 System.out.println("Type 'E' for encryption, or type 'D' for decryption");
+                scanner = new Scanner(System.in);
                 encrypt_decrypt = scanner.nextLine();
 
 
                 if(encrypt_decrypt.toLowerCase().equals("e"))
                 {
+                    //open file
                     file = fileOpener.openFile(System.in, System.out);
 
-//                    ClassLoader classLoader = getClass().getClassLoader();
-//                    File file = new File(classLoader.getResource("EncryptionAlgorithm.xml").getFile());
+                    //gets default algorithm
+                    AlgorithmFactory algorithmFactory = new AlgorithmFactory();
+                    encryption = algorithmFactory.getDefault();
+
+                    //create random key
+                    key = new ArrayList<>();
+                    key.add(KeyGen.randKey());
+
+                    //encryption
+                    encryptionResult = encryption.encrypt(fileToByteArray(file), key);
+
+                    //create encrypted file and "key.bin" file
+                    data = encryptionResult.getData();
+                    path = file.getAbsolutePath() + ".encrypted";//add .encrypted to end of file name
+
+                    fileCreator = new FileCreator();
+                    fileCreator.createFile(path, data);
+
+                    path = path.substring(0, path.lastIndexOf("\\")) + "\\key.bin";
+                    fileCreator.serializeKey(path, key);
 
                 }
                 else if (encrypt_decrypt.toLowerCase().equals("d"))
@@ -56,7 +90,7 @@ public class Driver {
                 // Encrypt/Decrypt a directory
                 break;
             case 3:
-                // Additional javatraining.algorithms
+                // Additional algorithms
                 break;
             case 4:
                 // Change default algorithm
@@ -81,5 +115,15 @@ public class Driver {
         {
             return false;
         }
+    }
+
+    public static byte[] fileToByteArray(File file)
+    {
+        try {
+            return Files.readAllBytes(file.toPath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
--- src/main/java/javatraining/tools/AlgorithmFactory.java
+++ src/main/java/javatraining/tools/AlgorithmFactory.java
@@ -1,6 +1,8 @@
 package javatraining.tools;
 
+import javatraining.algorithms.Caesar;
 import javatraining.designPatterns.Encryption;
+import javatraining.designPatterns.EncryptionBase;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,6 +15,9 @@ import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.URLDecoder;
 import java.util.List;
 
 /**
@@ -37,11 +42,16 @@ public class AlgorithmFactory {
 
         try
         {
-            SchemaFactory factory =
-                    SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-            Schema schema = factory.newSchema(new StreamSource(xsdFile));
+            String xmlPath = xmlFile.getAbsolutePath();
+            xmlPath = URLDecoder.decode(xmlPath, "UTF-8");
+            String xsdPath = xsdFile.getAbsolutePath();
+            xsdPath = URLDecoder.decode(xsdPath, "UTF-8");
+            InputStream xml = new FileInputStream(xmlPath);
+            InputStream xsd = new FileInputStream(xsdPath);
+            SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            Schema schema = factory.newSchema(new StreamSource(xsd));
             Validator validator = schema.newValidator();
-            validator.validate(new StreamSource(xmlFile));
+            validator.validate(new StreamSource(xml));
             return true;
         }
         catch(Exception ex)
@@ -62,10 +72,11 @@ public class AlgorithmFactory {
 
                 Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
                 EncryptionAlgorithm encryptionAlgorithm = (EncryptionAlgorithm) jaxbUnmarshaller.unmarshal(xmlFile);
-                System.out.println(encryptionAlgorithm);
+//                System.out.println(encryptionAlgorithm);
 
                 switch (encryptionAlgorithm.getAlgorithmName().toLowerCase()){
                     case "caesar":
+                        encryption = new Caesar(new EncryptionBase());
                         break;
                 }
 
