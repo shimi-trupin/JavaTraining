5af8648b7782350746b6aae7eb12b2a9ae757f0f
shimi <shimon.trupin1994@gmail.com>
2016-08-11 00:13:16 +0300

    injector async

--- src/main/java/javatraining/Driver.java
+++ src/main/java/javatraining/Driver.java
@@ -27,9 +27,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-/**
- * Created by shimi on 08/08/2016.
- */
 public class Driver {
 
     private static File file;
